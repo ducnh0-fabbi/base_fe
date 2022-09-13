868501761225cfdaae3fa561f9036642986ca219
DucNh-fabbi <ducnh@fabbi.io>
2022-09-13 14:34:04 +0700

    feat:list-jobs

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -2,13 +2,13 @@
   <div id="header" class="d-flex justify-content-between align-items-center">
     <div class="d-flex  align-items-center" @click="openSidebar()" >
       <img class="margin-right-12" v-if="MAIN_ROUTERS.includes(route.name)" :src="
-        require(`@/assets/image/logo-${route.name}.svg`)
+        require(`@/assets/images/logo-${route.name}.svg`)
       " alt="">
-      <p class="mb-0 font-weight-7 font-size-xl1">{{ $t(`sidebar.${route.name}`) }}</p>
+      <p class="mb-0 font-weight-7 font-size-24">{{ $t(`sidebar.${route.name}`) }}</p>
     </div>
     <div class="d-flex header--left">
-      <img src="@/assets/image/bell.svg" class="header--left-img" alt="">
-      <img src="@/assets/image/person.svg" alt="">
+      <img src="@/assets/images/bell.svg" class="header--left-img" alt="">
+      <img src="@/assets/images/person.svg" alt="">
     </div>
   </div>
   <div class="header-border d-flex w-100">
--- src/components/SidebarView.vue
+++ src/components/SidebarView.vue
@@ -2,20 +2,20 @@
   <div id="sidebar">
     <div class="sidebar-header text-center position-relative h-100">
       <div class="sidebar-logo">
-        <img src="@/assets/image/logo.svg" alt="">
+        <img src="@/assets/images/logo.svg" alt="">
       </div>
       <div v-for="(option, index) in OPTIONS_SIDEBAR" :key="index">
         <div class="option">
           <router-link :to="{ name: option.routerName }" >
             <div class="text-left f-w6 option__div d-flex justify-content-start align-items-center margin-bottom-2">
-              <img :src="require('@/assets/image/' + option.src)" />
+              <img :src="require('@/assets/images/' + option.src)" />
               {{ $t(`sidebar.${option.text}`) }}
             </div>
           </router-link>
         </div>
       </div>
       <div class="button-back position-absolute" @click="closeSidebar()">
-        <img src="@/assets/image/icon-back.svg" alt="icon_back">
+        <img src="@/assets/images/icon-back.svg" alt="icon_back">
       </div>
     </div>
   </div>
