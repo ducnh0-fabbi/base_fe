22d351ec556ce994babc0ae17f487a59f1d21695
DucNh-fabbi <ducnh@fabbi.io>
2022-09-14 13:56:20 +0700

    feat:job-manager

---
+++ src/components/JobDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="job-detail">
+    <p class="mb-0 font-size-18 font-weight-7">{{ props.job?.name }}</p>
+    <div class="d-flex job-detail__header align-items-center flex-wrap">
+      <span class="font-size-13 font-weight-4">{{ props.job?.company }}</span>
+      <div class="job-detail__header-dot margin-left-9 margin-right-9" />
+      <div class="job-detail__header-status">
+        {{ props.job?.status }}
+      </div>
+    </div>
+    <div class="job-detail__center d-flex flex-wrap margin-bottom-18">
+      <div class="job-detail__center--left margin-right-35">
+        <label for="time_request">{{ $i18n.t('label.time_request') }}</label>
+        <p class="font-size-18 font-weight-7">{{ props.job?.time_request }}</p>
+        <label for="amount">{{ $i18n.t('label.amount') }}</label>
+        <p class="font-size-18 font-weight-7">{{ props.job?.amount }}</p>
+      </div>
+      <div class="job-detail__center--right">
+        <label for="time_onboard">{{ $i18n.t('label.time_onboard') }}</label>
+        <p class="font-size-18 font-weight-7">{{ props.job?.time_onboard }}</p>
+        <label for="men_request">{{ $i18n.t('label.men_request') }}</label>
+        <p class="font-size-18 font-weight-7">{{ props.job?.men_request }}</p>
+      </div>
+    </div>
+    <router-link :to="{ name: 'Login' }" class="font-size-14 font-weight-5">{{ $i18n.t('label.review') }}</router-link>
+  </div>
+</template>
+
+<script setup>
+  // eslint-disable-next-line no-undef
+  const props = defineProps({
+    job: {
+      type: Object,
+      require: true,
+      description: 'Job object data',
+    }
+  });
+</script>
--- src/components/JobRhr.vue
+++ src/components/JobRhr.vue
@@ -171,17 +171,85 @@
           </div>
         </div>
       </div>
-      <div class="content_center">
-
+      <div class="content_center margin-top-10">
+        <p class="font-size-18 font-weight-7 mb-0 text-left">{{ $i18n.t('header.list_job') }}</p>
+        <div class="content_center-list-job d-flex justify-content-between text-left">
+          <div class="content_center-list-job__div">
+            <div class="d-flex justify-content-between align-items-center content_center-list-job__div-header margin-bottom-12">
+              <div class="d-flex align-items-center">
+                <div class="border __new"></div>
+                <p class="mb-0 font-size-15 font-weight-5">Open</p>
+              </div>
+              <div class="amount">
+                {{listJobs.jobOpens.length}}
+              </div>
+            </div>
+            <div class="content_center-list-job__div-detail" @drop="drop($event, STATUS_JOB_OPEN)" @dragover.prevent>
+              <template v-for="(jobOpen, index) in listJobs.jobOpens" :key="index">
+                <JobDetail draggable="true" @dragstart="drag($event, index, STATUS_JOB_OPEN)" :job="jobOpen" :class="index <= listJobs.jobOpens.length - 1 ? 'margin-bottom-12' : ''"/>
+              </template>
+            </div>
+          </div>
+          <div class="content_center-list-job__div">
+            <div class="d-flex justify-content-between align-items-center content_center-list-job__div-header margin-bottom-12">
+              <div class="d-flex align-items-center">
+                <div class="border __pending"></div>
+                <p class="mb-0 font-size-15 font-weight-5">Pending</p>
+              </div>
+              <div class="amount">
+                {{listJobs.jobPendings.length}}
+              </div>
+            </div>
+            <div class="content_center-list-job__div-detail"  @drop="drop($event, STATUS_JOB_PENDING)" @dragover.prevent>
+              <template v-for="(jobPending, index) in listJobs.jobPendings" :key="index">
+                <JobDetail draggable="true" @dragstart="drag($event, index, STATUS_JOB_PENDING)" :job="jobPending" :class="index <= listJobs.jobPendings.length - 1 ? 'margin-bottom-12' : ''"/>
+              </template>
+            </div>
+          </div>
+          <div class="content_center-list-job__div">
+            <div class="d-flex justify-content-between align-items-center content_center-list-job__div-header margin-bottom-12">
+              <div class="d-flex align-items-center">
+                <div class="border __doing"></div>
+                <p class="mb-0 font-size-15 font-weight-5">Doing</p>
+              </div>
+              <div class="amount">
+                {{listJobs.jobDoings.length}}
+              </div>
+            </div>
+            <div class="content_center-list-job__div-detail"  @drop="drop($event, STATUS_JOB_DOING)" @dragover.prevent>
+              <template v-for="(jobDoing, index) in listJobs.jobDoings" :key="index">
+                <JobDetail draggable="true" @dragstart="drag($event, index, STATUS_JOB_DOING)" :job="jobDoing" :class="index <= listJobs.jobDoings.length - 1 ? 'margin-bottom-12' : ''"/>
+              </template>
+            </div>
+          </div>
+          <div class="content_center-list-job__div">
+            <div class="d-flex justify-content-between align-items-center content_center-list-job__div-header margin-bottom-12">
+              <div class="d-flex align-items-center">
+                <div class="border __done"></div>
+                <p class="mb-0 font-size-15 font-weight-5">Done</p>
+              </div>
+              <div class="amount">
+                {{listJobs.jobDones.length}}
+              </div>
+            </div>
+            <div class="content_center-list-job__div-detail"  @drop="drop($event, STATUS_JOB_DONE)" @dragover.prevent>
+              <template v-for="(jobDOne, index) in listJobs.jobDones" :key="index">
+                <JobDetail draggable="true" @dragstart="drag($event, index, STATUS_JOB_DONE)" :job="jobDOne" :class="index <= listJobs.jobDones.length - 1 ? 'margin-bottom-12' : ''"/>
+              </template>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { reactive, ref } from 'vue';
-  import { TAB_ACTIVE, TAB_INACTIVE, LIST_COMPANIES } from '@/constants/job';
+  import { reactive, ref, computed } from 'vue';
+  import { TAB_ACTIVE, TAB_INACTIVE, LIST_COMPANIES, STATUS_JOB_OPEN, STATUS_JOB_PENDING, STATUS_JOB_DOING, STATUS_JOB_DONE } from '@/constants/job';
   import AppSelect from '@/components/AppSelect.vue';
+  import JobDetail from '@/components/JobDetail.vue';
+  import { useStore } from 'vuex';
 
   let activeTab = ref(TAB_ACTIVE);
 
@@ -197,6 +265,10 @@
     priority: null,
   });
 
+  const store = useStore();
+
+  const listJobs = computed(() => store.getters['job/listJobs']);
+
   const changeTab = (tab) => {
     if (activeTab.value != tab) {
       activeTab.value = tab
@@ -205,6 +277,24 @@
 
   const onSelect = (data, type) => {
     filter[type] = data;
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  const drag = (event, index, type) => {
+    event.dataTransfer.dropEffect = 'move';
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('itemIndex', index);
+    event.dataTransfer.setData('type', type);
+  }
+
+  const drop = (event, type) => {
+    const itemIndex = event.dataTransfer.getData('itemIndex');
+    const typeItem = event.dataTransfer.getData('type');
+    if (typeItem !== type) {
+      const item = listJobs.value[typeItem][itemIndex];
+      listJobs.value[type].push(item);
+      listJobs.value[typeItem].splice(itemIndex, 1);
+    }
   }
 
 </script>
